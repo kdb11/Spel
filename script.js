df2fb0b1cf9e6431ae7e71208c12c0d78816976c
kdb11 <kevin.badwi@medieinstitutet.se>
2022-11-30 13:57:45 +0100

    version 4

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 var characterJump = document.getElementById("character");
 var image = document.getElementById("charImg");
-
+var enemy = document.getElementById("object");
+var points = document.getElementById("points");
+var pts = 0;
 function changeImg () {
-    image.src="Images/jump.png";
-    setTimeout(function(){
-        image.src="Images/run.png"
-    }, 1000)
+    if(image.getAttribute('src') !== "Images/jump.png"){    
+        image.src="Images/jump.png";
+        setTimeout(function(){
+            image.src="Images/run.png"
+        }, 1000)
+    }
 }
 
 function jump () {
     console.log("hoppFunc");
-    characterJump.classList.add("animationJump");
-    setTimeout(function(){
-        characterJump.classList.remove("animationJump");
-    }, 1000)
+    if(!characterJump.classList.contains("animationJump")){
+        characterJump.classList.add("animationJump");
+        setTimeout(function(){
+            characterJump.classList.remove("animationJump");
+        }, 1000)
+    }
 }
 
 document.addEventListener("keyup", (e) => {
@@ -27,3 +33,19 @@ document.addEventListener("keyup", (e) => {
         break;
     }
 })
+
+var checkDead = setInterval(function(){
+    var characterTop = parseInt(window.getComputedStyle(characterJump).getPropertyValue("top"))
+    var blockLeft = parseInt(window.getComputedStyle(enemy).getPropertyValue("left"))
+    if(blockLeft<20 &&
+        blockLeft>0 &&
+        characterTop>=400){
+            window.location.reload();
+        }
+},10)
+
+var getPts = setInterval(function(){
+        pts++;
+        points.innerHTML = pts;
+},2000)
+
